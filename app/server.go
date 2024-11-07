d424ef30a9da617d326e61d6c000683691a0b902
Joao Pereira <joaop6775@gmail.com>
2024-11-07 15:56:41 +0000

    200/404

--- app/server.go
+++ app/server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -26,10 +28,68 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+	data, err := readAllData(conn)
+	if err != nil {
+		fmt.Println("Error reading connection data: ", err.Error())
+		os.Exit(1)
+	}
+
+	requestLine, _, _ := parseHttpRequest(data)
+	_, path, _ := parseRequestLine(requestLine)
+
+	// routing / handlers
+	var response []byte
+	if bytes.Equal(path, []byte("/")) {
+		response = []byte("HTTP/1.1 200 OK\r\n\r\n")
+	} else {
+		response = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+	}
+
+	_, err = conn.Write(response)
 	if err != nil {
 		fmt.Println("Error writing answer: ", err.Error())
 		os.Exit(1)
 	}
 
+}
+
+func readAllData(conn net.Conn) ([]byte, error) {
+	InBufferLen := 1024
+	inBuffer := make([]byte, InBufferLen)
+	outBuffer := new(bytes.Buffer)
+
+	for {
+		n, err := conn.Read(inBuffer)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("read error:", err) // todo error msg
+				return nil, err
+			}
+			break
+		}
+		outBuffer.Write(inBuffer[:n])
+		if n < InBufferLen {
+			break
+		}
+	}
+	return outBuffer.Bytes(), nil
+}
+
+func parseHttpRequest(data []byte) ([]byte, []byte, []byte) {
+	endOfReqLine := bytes.Index(data, []byte("\r\n")) + 1     // idx of \n
+	endOfHeaders := bytes.LastIndex(data, []byte("\r\n")) + 1 // idx of \n
+
+	requestLine := data[:endOfReqLine+1] // todo include \r\n on line or not?
+	headers := data[endOfReqLine+1 : endOfHeaders+1]
+	body := data[endOfHeaders+1:]
+	return requestLine, headers, body
+}
+
+func parseRequestLine(line []byte) (method, path, version []byte) {
+	components := bytes.Split(line, []byte(" "))
+	method = components[0]
+	path = components[1]
+	version = components[2]
+	return
 }
